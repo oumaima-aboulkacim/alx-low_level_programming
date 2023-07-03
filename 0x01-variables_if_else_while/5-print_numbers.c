18f5db2147d0acbf1606b28e4d54f89bd2c9ae8d
oumaima-aboulkacim <oumaymaaboulkacim@gmail.com>
2023-07-03 05:59:55 -0700

    Variables, if, else, while C

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prog entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prog
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while
+		(c < 10) {
+			putcar(c + '0');
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
